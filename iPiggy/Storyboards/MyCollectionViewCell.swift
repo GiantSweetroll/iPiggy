bbb554e48b1542dca7828c8658917e8751a4d58f
Bently Edyson <bentlyedyson@gmail.com>
2019-10-20 18:38:16 +0700

    fift storyboard

---
+++ iPiggy/Storyboards/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  iPiggy
+//
+//  Created by Bently Edyson on 20/10/19.
+//  Copyright © 2019 Gardyan Akbar. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
