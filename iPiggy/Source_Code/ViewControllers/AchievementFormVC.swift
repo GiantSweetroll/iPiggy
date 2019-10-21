93ac3db1cbc5ea31d3f200b227b811dac8e119e1
GiantSweetroll <gardyanakbar99@gmail.com>
2019-10-21 20:59:24 +0700

    Some edits of achievements from coding

---
+++ iPiggy/Source_Code/ViewControllers/AchievementFormVC.swift
@@ -0,0 +1,25 @@
+//
+//  AchievementForm.swift
+//  iPiggy
+//
+//  Created by Gardyan Akbar on 21/10/19.
+//  Copyright © 2019 Gardyan Akbar. All rights reserved.
+//
+
+import UIKit
+
+class AchievementFormVC: UIViewController
+{
+    //MARK: Storyboard Elements
+    @IBOutlet weak var tfWish: UITextField!
+    @IBOutlet weak var tfCost: UITextField!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        dismiss(animated: true)
+    }
+    
+    //MARK: - Methods
+    
+}
