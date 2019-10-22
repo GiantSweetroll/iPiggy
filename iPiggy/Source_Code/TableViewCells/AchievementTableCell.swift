be18c460dcc5d69b5c2f1fc014c458c5f52c9003
GiantSweetroll <gardyanakbar99@gmail.com>
2019-10-22 17:20:03 +0700

    Coding progress

---
+++ iPiggy/Source_Code/TableViewCells/AchievementTableCell.swift
@@ -0,0 +1,29 @@
+//
+//  AchievementTableCell.swift
+//  iPiggy
+//
+//  Created by Gardyan Akbar on 22/10/19.
+//  Copyright © 2019 Gardyan Akbar. All rights reserved.
+//
+
+import UIKit
+
+class AchievementTableCell: UITableViewCell
+{
+    //MARK: - IBOutlets
+    @IBOutlet weak var label: UILabel!
+    
+    //MARK: - Primary Methods
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
