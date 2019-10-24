327973e58c93c2a22a1ac527f012aaf7b3567574
GiantSweetroll <gardyanakbar99@gmail.com>
2019-10-25 05:38:13 +0700

    Finished with month collection view cell for yearly calendar

--- iPiggy/Source_Code/CollectionViewCells/YearMonthCalendarCVC.swift
+++ iPiggy/Source_Code/CollectionViewCells/YearMonthCalendarCVC.swift
@@ -8,7 +8,45 @@
 
 import UIKit
 
-class YearMonthCalendarCVC:UICollectionViewCell
+class YearMonthCalendarCVC:UICollectionViewCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
 {
     //MARK: - IBOutlets
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    //MARK: - Variables
+    var monthIndex:Int!
+    
+    //MARK: - Protocols
+    //tell the collection view how many cells to make
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return Globals.fullListOfCalendarDays[self.monthIndex].count
+    }
+    
+    //Make a cell for each cell index path
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        //get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CVC_CALENDAR_YEARLY_DAILY_CELL, for: indexPath as IndexPath) as! YearDayCalendarCVC
+        
+        //Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.dayLabel.text = Globals.fullListOfCalendarDays[self.monthIndex][indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - UICollectionViewDelegate protocol
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        //handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat        //Spacing between rows
+    {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat   //Spacing between columns
+    {
+        return 0
+    }
 }
--- iPiggy/Source_Code/ViewControllers/YearlyCalendarVC.swift
+++ iPiggy/Source_Code/ViewControllers/YearlyCalendarVC.swift
@@ -8,6 +8,39 @@
 
 import UIKit
 
-class YearlyCalendarVC: UIViewController
+class YearlyCalendarVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
 {
+    //MARK: - Protocols
+    //tell the collection view how many cells to make
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return Globals.fullListOfCalendarDays.count
+    }
+    
+    //Make a cell for each cell index path
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        //get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CVC_CALENDAR_MONTHLY_CELL, for: indexPath as IndexPath) as! YearMonthCalendarCVC
+        
+        //Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.monthLabel.text = Constants.MONTHS[indexPath.row]
+        cell.monthIndex = indexPath.row
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        //handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat        //Spacing between rows
+    {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat   //Spacing between columns
+    {
+        return 0
+    }
+
 }
